fc91c5ff40d87f3256eba6ba2e2af94caabd99d8
omswaroop2003 <omswaroop1986@gmail.com>
2023-11-03 15:34:05 +0530

    maven_java

---
+++ src/main/java/com/kmit/maven_java/App.java
@@ -0,0 +1,14 @@
+package com.kmit.maven_java;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+        System.out.println("Wlcome to kmit");
+    }
+}
